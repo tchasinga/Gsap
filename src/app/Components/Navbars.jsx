10cf9e534a4c0dbb66281b2c662f1795bbd50281
Tchasinga jacques <tchasingajacques@gmail.com>
2024-11-30 13:05:29 +0300

    feat: enhance Navbars component with dynamic menu and toggle functionality

--- src/app/Components/Navbars.jsx
+++ src/app/Components/Navbars.jsx
@@ -1,9 +1,28 @@
 "use client"
-import React from 'react'
+import React ,{useState , useEffect, useRef} from 'react'
 import Link from 'next/link'
 import './navdesign.css'
 
+const menu = [
+  {name: 'Home', link: '/'},
+  {name: 'About', link: '/about'},
+  {name: 'Contact', link: '/contact'},
+  {name: 'Services', link: '/services'},
+  {name: 'Blog', link: '/blog'},
+  {name: 'Portfolio', link: '/portfolio'},
+  {name: 'Shop', link: '/shop'},
+  {name: 'Elements', link: '/elements'},
+]
+
 export default function Navbars() {
+
+  const container = useRef()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className='famili'>
      
