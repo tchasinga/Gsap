b53bace41e12d84426f753e8d2b844f710a689b3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-11-30 12:47:41 +0300

    feat: update Navbars component with routing and styling

--- src/app/Components/Navbars.jsx
+++ src/app/Components/Navbars.jsx
@@ -1,5 +1,7 @@
 "use client"
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './navdesign.css'
 
 export default function Navbars() {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
-import Navbars from "./Components/Navbars";
 
 export default function Home() {
   return (
     <div className="">
-       <Navbars />
+
     </div>
   );
 }
